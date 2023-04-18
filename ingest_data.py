2352838e1532b0d6e3b14f4d37090abb4a20abd9
nipdep <nipun1deelaka@gmail.com>
2023-04-18 10:24:20 +0530

    [update] to LLama Embedding

--- ingest_data.py
+++ ingest_data.py
@@ -1,12 +1,13 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import ReadTheDocsLoader
 from langchain.vectorstores.faiss import FAISS
-from langchain.embeddings import OpenAIEmbeddings
+# from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import LlamaCppEmbeddings
 import pickle
 
 # Load Data
 
-def scrapeAndSave(url="langchain.readthedocs.io", out_path="vectorstore.pkl"):
+def scrapeAndSave(url="langchain.readthedocs.io", out_path="vectorstore.pkl", model_path="/path/to/model/ggml-model-q4_0.bin"):
     loader = ReadTheDocsLoader(url)
     raw_documents = loader.load()
 
@@ -16,7 +17,7 @@ def scrapeAndSave(url="langchain.readthedocs.io", out_path="vectorstore.pkl"):
 
 
     # Load Data to vectorstore
-    embeddings = OpenAIEmbeddings()
+    embeddings = LlamaCppEmbeddings(model_path=model_path)
     vectorstore = FAISS.from_documents(documents, embeddings)
 
 
